2da0ce36a7eb270cde88ba863f84d7f0b14fc9b8
behzod <behzodme@gmail.com>
2022-05-14 12:22:27 +0500

    behzod

--- project/settings.py
+++ project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9)a&-+q)bmn+7t@+-yu&_2tc^wck*jia*+69)@6$3gp)ob^ucn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
